e34573f7beec9a7a15309934139af3238af0e2df
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2023-12-05 17:44:09 -0500

    Add files via upload

--- MLP.py
+++ MLP.py
@@ -11,19 +11,18 @@ from scipy.io import loadmat
 button = loadmat('behavioralDataPerEphysSample_samplingFrequency2000Hz_run2.mat')
 
 #User X-pos
-uxp = button['unityData'][0][0][0][0]
+uxpog = button['unityData'][0][0][0][0]
 #User Y-Pos
-uyp = button['unityData'][0][0][1][0]
+uypog = button['unityData'][0][0][1][0]
 
-#Set the number of trials that we do per epoch (size of training set)
-data_points = 2000
 
 #Start of velocity task
 instruction_switch = np.where(button['unityData'][0][0][3][0] == 1)[0][0]
 
+
 #Get the user position
-uxp = uxp[instruction_switch:instruction_switch+data_points]
-uyp = uyp[instruction_switch:instruction_switch+data_points]
+uxp = uxpog[instruction_switch:len(uypog)-1]
+uyp = uypog[instruction_switch:len(uypog)-1]
 
 #Calculate instantaneous velocity
 dxp = uxp[1:len(uxp)-1]-uxp[0:len(uxp)-2]
@@ -31,17 +30,57 @@ dyp = uyp[1:len(uyp)-1]-uyp[0:len(uyp)-2]
 
 vel = np.sqrt(dxp**2 + dyp**2)*2000  #2000 is sampling rate = 1/s
 
+print(len(vel))
+
+vel = vel[0:490000:1000]
+
+#TEMPORARY
+
+#vel = vel[0]
+vel2 = []
+for kl in vel:
+    if kl >= 1.561:
+        vel2.append(1)
+    else:
+        vel2.append(0)
+        
+
+'''#side velocitu calc
+uxp2 = uxpog[instruction_switch-1:instruction_switch+1+200000]
+uyp2 = uypog[instruction_switch-1:instruction_switch+1+200000] 
+dxp2 = uxp2[1:len(uxp2)-1]-uxp2[0:len(uxp2)-2]
+dyp2 = uyp2[1:len(uyp2)-1]-uyp2[0:len(uyp2)-2]
+
+vel2 = np.sqrt(dxp2**2 + dyp2**2)*2000'''
+
+plt.figure(1)
+plt.plot(vel)
+
 #Bring in the LFP data
 data_holder = []
 num_channels = 5
 for iiii in range(89,89+num_channels):
     chan = loadmat(f'CSC{iiii}.mat')
     data = chan['data'][0]
-    data = data[instruction_switch:instruction_switch+data_points]
+    data = data[instruction_switch+1:len(data)-2]
+    #print(len(data))
     #Intra channel normalization maybe try whole dataset normalization later
-    data = (data - np.min(data)) / (np.max(data) - np.min(data))
-    data_holder.append(data)
+    #data = (data - np.min(data)) / (np.max(data) - np.min(data))
+    data = data[0:490000:1000]
     
+    #TEMPORARY
+    #data = data[0]
+    data_holder.append(data)
+
+#Try normalizing based on everything (might help with KL loss)
+flat_data = [item for sublist in data_holder for item in sublist]
+
+min_value = min(flat_data)
+max_value = max(flat_data)
+
+data_holder = [[(x - min_value) / (max_value - min_value) for x in row] for row in data_holder]
+
+
 data_holder = np.array(data_holder)
 input_data = data_holder.T
 
@@ -61,9 +100,9 @@ input_data = data_holder.T
 
 #Inner size of encoder and decoder ###MUST MATCH
 #This might be a mistake
-encoder_sizes = [5,4,4,3]
+encoder_sizes = [num_channels,4,4,3]
 latent_space_size = 2
-decoder_sizes = [3,4,4,5]
+decoder_sizes = [3,4,4,num_channels]
 
 def make_net(encoder_sizes,latent_space_size,decoder_sizes):  
     #Initilize the network to have random weights between 0 and 1.
@@ -185,7 +224,13 @@ def forward(sec_dat, mlp_weights,encoder_sizes,latent_space_size,decoder_sizes):
 #xis = forward(sec_dat,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes)
 
 
-def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
+def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta,kl_lambda, epochs,tracker):
+
+    #print('mlp below')
+    #print(mlp_weights)
+    #if tracker == epochs -1:
+        #print('xis below')
+        #print(xis)
 
     #To start I am just going to be looking at the reconstruction loss. I am not 100% sure yet if we need to incorperate the
     #other component of loss or if this is good enough.
@@ -193,7 +238,7 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
     
     loss_pre = []
     for m in range(len(xis[-1])):
-        loss_mini = (xis[-1][m] - xis[0][m])
+        loss_mini = (xis[-1][m] - xis[0][m])**2
         loss_pre.append(loss_mini)
         
     loss_out = sum(loss_pre)
@@ -206,21 +251,18 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
     #Get the output deltas
     deltas = []
     output_deltas = []
+    
+    #print('\n\n')
+    #print('Values within the cells (xis)')
+    #print(xis)
+    #print('\n\n')
+    
+    
     for m in range(len(xis[-1])):
         delta = (xis[-1][m] - xis[0][m])*xis[-1][m]*(1-xis[-1][m])
         output_deltas.append(delta)
     deltas.append(output_deltas)
-
-
-
-#        #              #
-#   #    #     #   #    #    
-#   #          #   #    #
-#   #    #         #    #
-#        #              #
-
-   
-        
+    
     #Update weights and get new deltas
     for m in range(len(decoder_sizes)):
         layer = mlp_weights[len(decoder_sizes)+len(encoder_sizes)-1-m]
@@ -243,47 +285,73 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
             for q in range(len(xis[-2-m])):
                 prev_dels = np.array(deltas[0+m])
                 wjk = []
+                print(len(layer))
                 for b in range(len(layer)):
                     wjk.append(mlp_weights[len(decoder_sizes)+len(encoder_sizes)-1-m][b][q])
                 wjk = np.array(wjk)
                 
                 sum_calc = sum(prev_dels*wjk)
-                new_delta = sum_calc*xis[-2-m][q]*xis[-1-m][k]*(1-xis[-1-m][k])#*Here there should be oj(1-oj)? (Should be good now)
+                new_delta = sum_calc*xis[-1-m][k]*(1-xis[-1-m][k])#*Here there should be oj(1-oj)? (Should be good now)
                 hidden_deltas.append(new_delta)
                 
                 
             deltas.append(hidden_deltas)
         
+    
+    
+    #print('Values between the cells weights (wis)')
     #print(mlp_weights)
-    #print('\n\n')
-    #print(deltas)
-    #print('\n\n')
-    #print(xis)
-    
-    
-        
-    #KL loss is really only relavent in the encoder so we are going to keep it here
-    #Now we have to update the weights on the super special layers that go into the mu and sigma
-    #delta of pervious*partial of KL loss*input node to mu/sigma layer
-    
-    #Start with sigma connected nodes
-    #Might be missing a term in previous deltas??? for sigmoids...
-    #print('spacer')
-    #print(deltas)
-    #print('spacer2')
     
     sig_layer = mlp_weights[len(encoder_sizes)-1][0]
+    #print(len(sig_layer))
+    
+    super_delta_guy = [] 
+    
     for m in range(len(sig_layer)):
         weight_sets = sig_layer[m]
+       
+        xiwis = []
+        for k in range(len(weight_sets)):
+            xiwis.append(mlp_weights[len(encoder_sizes)][k][m]*deltas[-1][k])
+              
+        #print(((1/xis[-len(decoder_sizes)-2][0][m])-xis[-len(decoder_sizes)-2][0][m]))
+        #print(xiwis)   
+        
+        xiwis = sum(xiwis)
+              
+        super_delta_guy.append(((1/xis[-len(decoder_sizes)-2][0][m])-xis[-len(decoder_sizes)-2][0][m])*xiwis)
+        
+       
+        #print(len(weight_sets))
         for k in range(len(weight_sets)):
             #print(eta*deltas[-1][k]*(xis[-len(decoder_sizes)-2][0][m]))
-            mlp_weights[len(encoder_sizes)-1][0][m][k] = mlp_weights[len(encoder_sizes)-1][0][m][k] - eta*deltas[-1][k]*(xis[-len(decoder_sizes)-2][0][m]*(1-xis[-len(decoder_sizes)-2][0][m]))*((-1/xis[-len(decoder_sizes)-2][0][m])+2*xis[-len(decoder_sizes)-2][0][m])*xis[-len(decoder_sizes)-3][k]
+            #mlp_weights[len(encoder_sizes)-1][0][m][k] = mlp_weights[len(encoder_sizes)-1][0][m][k] - eta*deltas[-1][k]*(xis[-len(decoder_sizes)-2][0][m]*(1-xis[-len(decoder_sizes)-2][0][m]))*((-1/xis[-len(decoder_sizes)-2][0][m])+2*xis[-len(decoder_sizes)-2][0][m])*xis[-len(decoder_sizes)-3][k]
+            #No activation function here... already accounted for in previous delta
+            #Switched the signs of sig portion bc KL should be positive in our scenario
+            #Need to add in weight
+            #print(mlp_weights[len(encoder_sizes)-1][0][m][k])
+            
+            cur_weights = []
+            for k in range(len(weight_sets)):
+                cur_weights.append(mlp_weights[len(encoder_sizes)][k][m])
+                
+            cur2_weights = sum(cur_weights)
+            
+            
+            
+            
+            mlp_weights[len(encoder_sizes)-1][0][m][k] = mlp_weights[len(encoder_sizes)-1][0][m][k] - eta*deltas[-1][k]*((1/xis[-len(decoder_sizes)-2][0][m])-xis[-len(decoder_sizes)-2][0][m])*xis[-len(decoder_sizes)-3][k]*cur2_weights#*(mlp_weights[len(encoder_sizes)][k][m]+
+            
     
-    #New deltas calculated are going to follow this layer
+   
+    
+    
+    
+    '''#New deltas calculated are going to follow this layer
     after_sampling_deltas_sig = []
     for k in range(len(weight_sets)):
-        delta = deltas[-1][k]*(xis[-len(decoder_sizes)-2][0][m]*(1-xis[-len(decoder_sizes)-2][0][m]))*((-1/xis[-len(decoder_sizes)-2][0][m])+2*xis[-len(decoder_sizes)-2][0][m])
-        after_sampling_deltas_sig.append(delta)
+        delta = kl_lambda*deltas[-1][k]*(xis[-len(decoder_sizes)-2][0][m]*(1-xis[-len(decoder_sizes)-2][0][m]))*((-1/xis[-len(decoder_sizes)-2][0][m])+2*xis[-len(decoder_sizes)-2][0][m])
+        after_sampling_deltas_sig.append(delta)'''
         
     #deltas.append(after_sampling_deltas)
     
@@ -292,25 +360,39 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
     for m in range(len(sig_layer)):
         weight_sets = sig_layer[m]
         
-        #print('hello')
-        #print(weight_sets)
-        #print('goodbye')
+        
+        xiwis = []
+        for k in range(len(weight_sets)):
+            xiwis.append(mlp_weights[len(encoder_sizes)][k][m]*deltas[-1][k])
+            
+        xiwis = sum(xiwis)
+              
+        super_delta_guy.append(-xis[-len(decoder_sizes)-2][1][m]*xiwis)
+        
         
         for k in range(len(weight_sets)):
-            mlp_weights[len(encoder_sizes)-1][1][m][k] = mlp_weights[len(encoder_sizes)-1][1][m][k] - eta*deltas[-1][k]*(xis[-len(decoder_sizes)-2][1][m]*(1-xis[-len(decoder_sizes)-2][1][m]))*((-1/xis[-len(decoder_sizes)-2][1][m])+2*xis[-len(decoder_sizes)-2][1][m])*xis[-len(decoder_sizes)-3][k]
+        
+            cur_weights = []
+            for k in range(len(weight_sets)):
+                cur_weights.append(mlp_weights[len(encoder_sizes)][k][m])
+                
+            cur2_weights = sum(cur_weights)
+        
+            mlp_weights[len(encoder_sizes)-1][1][m][k] = mlp_weights[len(encoder_sizes)-1][1][m][k] - eta*deltas[-1][k]*-xis[-len(decoder_sizes)-2][1][m]*xis[-len(decoder_sizes)-3][k]*cur2_weights
     
-    #New deltas calculated are going to follow this layer
+    '''#New deltas calculated are going to follow this layer
     after_sampling_deltas_mu = []
     for k in range(len(weight_sets)):
-        delta = deltas[-1][k]*(xis[-len(decoder_sizes)-2][1][m]*(1-xis[-len(decoder_sizes)-2][1][m]))*((-1/xis[-len(decoder_sizes)-2][1][m])+2*xis[-len(decoder_sizes)-2][1][m])
-        after_sampling_deltas_mu.append(delta)
+        delta = kl_lambda*deltas[-1][k]*(xis[-len(decoder_sizes)-2][1][m]*(1-xis[-len(decoder_sizes)-2][1][m]))*((-1/xis[-len(decoder_sizes)-2][1][m])+2*xis[-len(decoder_sizes)-2][1][m])
+        after_sampling_deltas_mu.append(delta)'''
+      
     
     
-    after_sampling_deltas_mu = np.array(after_sampling_deltas_mu)
+    '''after_sampling_deltas_mu = np.array(after_sampling_deltas_mu)
     after_sampling_deltas_sig= np.array(after_sampling_deltas_sig)
     
     adder_thing = (after_sampling_deltas_sig+after_sampling_deltas_mu).tolist() 
-    deltas.append(adder_thing)
+    deltas.append(adder_thing)'''
     
     #Now we have to take care of updating the weights in the encoder.
     #Need to calculate deltas... just in case we add more layers to encoder
@@ -325,8 +407,42 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
     #How we gunna do that?
     #Well the first layer was already done
     #We need encoder size - 1 times to do this
+    
+    
+    #Special layer accounting for 
+    #In other words get new deltas
+    
+    hidden_deltas = []
+    for q in range(len(xis[-len(decoder_sizes)-3])):
+        #Super_delta_guy
+        #prev_dels = np.array(deltas[0+m])
+        wjk = []
+        #print(len(layer))
+        flat_guy = []
+        for jk in range(latent_space_size):
+            
+            for tl in mlp_weights[-len(decoder_sizes)-1][jk]:
+                flat_guy.append(tl)
+   
+        
+        #flat_guy = mlp_weights[-len(decoder)-1][0] + mlp_weights[-len(decoder)-1][1]
+        
+        for b in range(len(super_delta_guy)):
+            wjk.append(flat_guy[b][q])
+        wjk = np.array(wjk)
+        #print('flat guy')
+        #print(flat_guy)
+        
+        sum_calc = sum(super_delta_guy*wjk)
+        new_delta = sum_calc*xis[-1-m][k]*(1-xis[-1-m][k])#*Here there should be oj(1-oj)? (Should be good now)
+        hidden_deltas.append(new_delta)
+    
+    deltas.append(hidden_deltas)
+    #print('deltas')
+    #print(deltas)
+    
     for m in range(len(encoder_sizes)-1):
-        print('askldjfha;lsdfh;alksjdfasf')
+        #print('askldjfha;lsdfh;alksjdfasf')
         #Going from back to front
         layer = mlp_weights[len(encoder_sizes)-2-m]
         for k in range(len(layer)):
@@ -397,7 +513,8 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
             #print(xis[-len(encoder_sizes)-4-m][q])
             
             #Recon delta, h (value we are going to), (1-prev)(prev)
-            new_delta = sum_calc*xis[-len(decoder_sizes)-4-m][q]*xis[-len(decoder_sizes)-3-m][k]*(1-xis[-len(decoder_sizes)-3-m][k])#*Here there should be oj(1-oj)? (Should be good now)
+            
+            new_delta = sum_calc*xis[-len(decoder_sizes)-3-m][k]*(1-xis[-len(decoder_sizes)-3-m][k])#*Here there should be oj(1-oj)? (Should be good now)
             hidden_deltas.append(new_delta)
             
             
@@ -418,46 +535,77 @@ def backprop(xis,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta):
 
 mlp_weights = make_net(encoder_sizes,latent_space_size,decoder_sizes)
 
-#Start training
-
-#print('input')
-#print(input_data[0])
+kl_lambda = 1
 
 #learning rate
-eta = 0.005
+eta = 0.001
 
 #Set number of epochs
-epochs = 1000
+epochs = 100
 
 loss_track_over_epoch = []
 
+tracker = 0
+
 for pocs in range(epochs):
     recon_loss = []
-    for lk in range(len(data_holder)):
+    graph_data = []
+    for lk in range(len(data_holder[0])):
         e_input = input_data[lk]
         #Do forwards propegation
         xis = forward(e_input,mlp_weights,encoder_sizes,latent_space_size,decoder_sizes)
+        
+        print(xis)
+        
+        if pocs == epochs-1:
+           graph_data.append(xis[-len(decoder_sizes)-1])
+           #print(xis[-len(decoder_sizes)-1])
+            
         #Do backwards propegation
-        loss_out = backprop(xis, mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta)
+        loss_out = backprop(xis, mlp_weights,encoder_sizes,latent_space_size,decoder_sizes,eta,kl_lambda, epochs, tracker)
         #Check the loss from backprop
         recon_loss.append(loss_out)
+    tracker = tracker + 1
     loss_track_over_epoch.append(sum(recon_loss))
 
+#print(np.shape(data_holder))
+#print(len(data_holder))
+#print(np.shape(graph_data))
 
 print('Looking at the reconstruction loss as the network optimizes')
 print(loss_track_over_epoch)
 
+graph_data = np.array(graph_data).T
 
-#Problem: Reconstruction loss does not seem to
-#         be getting much better between trials
-#Ideas
+colors = vel
+# Create a scatter plot with colormap
+fig = plt.figure(2)
+#ax = fig.add_subplot(projection='3d')
+#ax.scatter(graph_data[0], graph_data[1], graph_data[2], c=colors, cmap='viridis', edgecolors='k', alpha=0.7)
+plt.scatter(graph_data[0], graph_data[1], c=colors, cmap='viridis', edgecolors='k', alpha=0.7)
 
-#1. Go back to smaller dataset         (done)
-#2. Try more layers (fix hardcoding)   (probably done)
-#3. Try smaller layer sizes
-#4. Try larger layer sizes
-#5. Try smaller etas
-#6. Try larger etas
-#7. Try more epochs
-#8. Try less epochs
+# Add a colorbar to show the mapping of colors
+cbar = plt.colorbar()
+cbar.set_label('Color Intensity')
+
+# Customize the plot with labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Scatter Plot with Colormap')
+
+# Show the plot
+plt.show()
+
+
+
+
+
+
+#To do's
+#1. Get a latent representation
+    #a. Button presses
+    #b. Velocity
+#2. If it looks really bad look at KL loss
+#3. Do GLM
+
   
