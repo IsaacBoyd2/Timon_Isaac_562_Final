52e6709aa27aa1fa10560d1eab2cf85769cdbd76
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2023-12-07 12:06:15 -0500

    Add files via upload

---
+++ vel_reg.py
@@ -0,0 +1,7 @@
+#Goals
+
+#1. Useing the raw data perform regression using MSE error loss function. (Can just take the encoder from the prvious model to do this)
+#2a. Try using PCA as input
+#2b. Try using VAE as input
+
+#3. Record and report results.
